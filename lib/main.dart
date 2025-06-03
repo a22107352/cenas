54eec977f7678acc56693bccb24aed0c32a0e07d
a22107352 <henriquemogf@gmail.com>
2025-06-03 19:27:55 +0100

    api e bd a funcionar

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:prjectcm/connectivity_module.dart';
 import 'ConnectivityModuleMeu.dart';
-import 'data/HospitalRepository.dart';
 import 'data/http_sns_datasource.dart';
 import 'data/sqflite_sns_datasource.dart';
 import 'main_screen.dart';
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   final sqfliteSnsDataSource = SqfliteSnsDataSource();
-  await sqfliteSnsDataSource.init();
-
   final httpSnsDataSource = HttpSnsDataSource();
-
-
-
+  final connectivityModule = ConnectivityModuleMeu();
 
   runApp(
-      MultiProvider(
-        providers: [
-          Provider<HttpSnsDataSource>.value(value: httpSnsDataSource),
-          Provider<SqfliteSnsDataSource>.value(value: sqfliteSnsDataSource),
-          Provider<ConnectivityModule>.value(value: ConnectivityModuleMeu()),
-        ],
-        child: MyApp(),
-      )
+    MultiProvider(
+      providers: [
+        Provider<HttpSnsDataSource>.value(value: httpSnsDataSource),
+        Provider<SqfliteSnsDataSource>.value(value: sqfliteSnsDataSource),
+        Provider<ConnectivityModule>.value(value: connectivityModule),
+      ],
+      child: const MyApp(),
+    ),
   );
 }
+
 
 
 
@@ -40,24 +36,47 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    var colorScheme = ColorScheme.fromSeed(seedColor: Colors.blueAccent);
+  late final SqfliteSnsDataSource sqfliteSnsDataSource;
 
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: colorScheme,
-        useMaterial3: true,
-        appBarTheme: ThemeData.from(colorScheme: colorScheme)
-            .appBarTheme
-            .copyWith(
-          backgroundColor: colorScheme.primary,
-          foregroundColor: colorScheme.surface,
-        ),
-      ),
-      title: 'Projecto CM',
-      home: Mainpage(),
-    );
+  @override
+  void initState() {
+    super.initState();
+    sqfliteSnsDataSource = context.read<SqfliteSnsDataSource>();
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<void>(
+      future: sqfliteSnsDataSource.init(),
+      builder: (_, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return const MaterialApp(
+            home: Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            ),
+          );
+        }
+
+        var colorScheme = ColorScheme.fromSeed(seedColor: Colors.blueAccent);
+
+        return MaterialApp(
+          title: 'Projecto CM',
+          theme: ThemeData(
+            colorScheme: colorScheme,
+            useMaterial3: true,
+            appBarTheme: ThemeData.from(colorScheme: colorScheme)
+                .appBarTheme
+                .copyWith(
+              backgroundColor: colorScheme.primary,
+              foregroundColor: colorScheme.surface,
+            ),
+          ),
+          home: Mainpage(),
+        );
+      },
+    );
+  }
 }
+
+
+
